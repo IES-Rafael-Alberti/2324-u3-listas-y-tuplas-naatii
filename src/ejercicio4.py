cbe0a8f5a46297f0960d67c4421019247bca4c86
naatii <natiworlds@gmail.com>
2023-11-01 18:23:19 +0100

    Hasta ejercicio 4

---
+++ src/ejercicio4.py
@@ -0,0 +1,29 @@
+def ordenarNumeros(numeros:list)->list:
+    numeros.sort()
+    return numeros
+
+# todo: intentar implementar esto
+# def comprobarLista(numeros:list, numero:int):
+#     if numero not in numeros:
+#         return True
+#     else:
+#         return False
+
+# ? Qué debería ir en una función y que no?
+
+def comprobarNumero(numero:str)->bool:
+    if numero.strip().isdigit():
+        return True
+    else:
+        return False
+
+if __name__=="__main__":
+    numeros = []
+    TAMAÑOLISTA = 6
+    while len(numeros)<TAMAÑOLISTA:
+        numero = input("Introduce un número de la secuencia ganadora: ")
+        if comprobarNumero(numero):
+            numeros.append(int(numero))
+        else:
+            print("ERROR: Introduce un número de la secuencia ganadora")
+    print(ordenarNumeros(numeros))
