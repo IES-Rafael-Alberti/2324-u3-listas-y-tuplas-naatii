ccad0b47d3de9b26e0b09a14c7c4c68b720eca87
naatii <natiworlds@gmail.com>
2023-11-04 11:50:33 +0100

    Arreglado fallo en ejercicio9

--- src/ejercicio9.py
+++ src/ejercicio9.py
@@ -1,4 +1,4 @@
-def contarPalabras(frase:str, vocal:str)->int:
+def contarLetras(word:str)->int:
     """Escribir un programa que pida al usuario una palabra y muestre por pantalla el número de veces que contiene cada vocal.
 
     Args:
@@ -7,13 +7,15 @@ def contarPalabras(frase:str, vocal:str)->int:
     Returns:
         int: El número de veces que contiene cada vocal en la palabra ingresada por el usuario.
     """
-    contador = 0
-    for letra in frase.lower():
-        if letra == vocal.lower():
-            contador += 1
-    return contador
+    times = 0
+    for letter in word: 
+        if letter == vocal:
+            times += 1
+    return times
 
 if __name__=="__main__":
-    frase = input("Ingrese una palabra: ")
-    vocal = input("Ingrese una vocal: ")
-    print(contarPalabras(frase, vocal))
+    word = input("Introduce una palabra: ")
+    vocals = ['a', 'e', 'i', 'o', 'u']
+    for vocal in vocals: 
+        times = contarLetras(word)
+        print("La vocal " + vocal + " aparece " + str(times) + " veces")
